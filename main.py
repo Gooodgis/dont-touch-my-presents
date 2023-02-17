cf83324b3ab6edf81e391c024767e835869efa61
greatfiredragon <kulmike2002@gmail.com>
2023-02-17 23:10:12 +0300

    changing everything to try exporting on android

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 
+
 from src.components.game_status import GameStatus
 from src.config import Config
 from src.game_phases import main_menu_phase, gameplay_phase, exit_game_phase
@@ -9,7 +10,6 @@ from src.services.music_service import MusicService
 pygame.init()
 
 FramePerSec = pygame.time.Clock()
-
 
 def update_game_display():
     pygame.display.update()
